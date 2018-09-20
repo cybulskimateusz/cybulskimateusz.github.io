cb69bba7130e9f22a76cb4b14c27bc214dec9ca9
Mateusz Cybulski <cybulskimateusz.work@icloud.com>
2018-09-20 22:47:32 +0200

    (#list-portfolio > li)size bug fix ; ivisible submit

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 		I put all of private, or public projects that I've, or I was a part of in here. Apps, Websites, and more...">
 		<meta name="keywords" content="showcase, mateusz, cybulski, deveroper, apps">
 		<meta name="author" content="Mateusz Cybulski">
-		<meta name="viewport" content="device-width, initial-scale=1.0" />
 
 		
 		<meta http-equiv="X-Ua-Compatible" content="IE=edge, chrome=1">
@@ -55,6 +54,7 @@
 				<div id="content-portfolio">
 					<ul id="list-portfolio">
 						<li></li>
+						<li></li>
 					</ul>
 					<div id="next" class="circle" >&rang;</div>
 				</div>
@@ -62,19 +62,20 @@
 			<article  id="contact">
 				<div class="panel">
 					<div class="contact-form">
-						<form id="form">
+						<form id="form" action="php/send.php">
 						<div class="inputs">
-							<input type="text" id="f-name" placeholder="name"></input>
-							<input type="text" id="f-mail" placeholder="e-mail"></input>
+							<input type="text" id="f-name" placeholder="name"/>
+							<input type="text" id="f-mail" placeholder="e-mail"/>
 						</div>
 							<textarea id="f-contact" placeholder="What's up?"></textarea>
+							<input type="submit" value="send" style="display:none !important;"/>
 						</form>
 					</div>
 					<div class="social-media">
 						<a target="_blank" title="LinkedIn" href="http://www.linkedin.com/in/mateusz-cybulski"><div class="circle" id="linkedin"><i class="demo-icon icon-linkedin"></i></div></a>
 						<a target="_blank" title="Facebook Messenger" href="https://m.me/i.am.m4tth3w"><div class="circle" id="messenger"><i class="demo-icon icon-comment"></i></div></a>
 						<a target="_blank" title="Twitter" href="https://twitter.com/@MateuszCybulsky"><div class="circle" id="twitter"><i class="demo-icon icon-twitter"></i></div></a>
-						<div class="circle" id="send" title="send" onclick="send()"><i class="demo-icon icon-mail"></i></div>
+						<div class="circle" id="send" title="send"><i class="demo-icon icon-mail"></i></div>
 					</div>
 				</div>
 			</article>
