d35691bf320370528cfac100593337fb8be6f716
Mateusz Cybulski <cybulskimateusz.work@icloud.com>
2018-09-22 13:24:21 +0200

    clear

--- script.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($){
-				$.scrollTo(0);
-				$('#logo').click(function() { $.scrollTo($('#now'),500);});
-				$('#menu-now').click(function() { $.scrollTo($('#now'),500);});
-				$('#menu-about').click(function() { $.scrollTo($('#about'),500);});
-				$('#menu-portfolio').click(function() { $.scrollTo($('#portfolio'),500);});
-				$('#menu-contact').click(function() { $.scrollTo($('#contact'),500);});
-			});
