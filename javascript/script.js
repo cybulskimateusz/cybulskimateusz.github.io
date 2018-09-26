a0d5e9bc6c6fb066e333b8d2508c9faf2c9791e7
Mateusz Cybulski <cybulskimateusz.work@icloud.com>
2018-09-26 15:35:49 +0200

    form for git

--- javascript/script.js
+++ javascript/script.js
@@ -5,5 +5,29 @@ jQuery(function($){
 				$('#menu-about').click(function() { $.scrollTo($('#about'),500);});
 				$('#menu-portfolio').click(function() { $.scrollTo($('#portfolio'),500);});
 				$('#menu-contact').click(function() { $.scrollTo($('#contact'),500);});
-				$('#send').click(function(){$('#form').submit();});
+				
+            function openMail(){
+					var q = confirm("Unfortunately the github.com doesn't support php. Do you want to send me an e-mail by a native app?");
+					if(q==true)
+						window.open('mailto:cybulskimateusz.work@icloud.com');
+						}
+			
+			$('#send').click(function(){
+					var q = confirm("Unfortunately the github.com doesn't support php. Do you want to send me an e-mail by a native app?");
+					if(q==true)
+						window.open('mailto:cybulskimateusz.work@icloud.com');
+					});
+			$('input:text').focus(function(){
+					var q = confirm("Unfortunately the github.com doesn't support php. Do you want to send me an e-mail by a native app?");
+					if(q==true)
+						window.open('mailto:cybulskimateusz.work@icloud.com');
+					});
+			$('textarea').focus(function(){
+					var q = confirm("Unfortunately the github.com doesn't support php. Do you want to send me an e-mail by a native app?");
+					if(q==true)
+						window.open('mailto:cybulskimateusz.work@icloud.com');
+					});
 			});
+
+			
+				
